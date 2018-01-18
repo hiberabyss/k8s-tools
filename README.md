3d15c2ba5205e4c949efe67cd7047fa88968144a
Hongbo Liu <hbliu@freewheel.tv>
2018-01-18 14:59:20 +0800

    initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+# What is this
+
+A shell script which helps to install following tools:
+
+* latest `kubectl`
+* `kns` and `kctx` from https://github.com/ahmetb/kubectx
+
+# How to install
+
+Simply run `sh -c "$(wget -O- https://raw.githubusercontent.com/hiberabyss/k8s-tools/master/install.sh")`
---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# Author            : Hongbo Liu <hbliu@freewheel.tv>
+# Date              : 2018-01-18
+# Last Modified Date: 2018-01-18
+# Last Modified By  : Hongbo Liu <hbliu@freewheel.tv>
+
+dest_dir="$HOME/bin"
+
+prepare_path() {
+    mkdir -p $dest_dir
+
+    if [[ ! "$PATH" =~ $dest_dir ]]; then
+        export PATH=$PATH:$dest_dir
+        echo 'export PATH=$PATH:$HOME/bin' | tee -a ~/.bashrc ~/.zshrc
+    fi
+}
+
+do_exist() {
+    if which $1 &> /dev/zero; then
+        return 0
+    fi
+    return 1
+}
+
+install_kubectl() {
+    kubectl_bin="$1"
+    sudo wget -O $kubectl_bin "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo chmod +x $kubectl_bin
+}
+
+precheck() {
+    if ! do_exist git; then
+        sudo yum install -y git
+    fi
+
+    if ! do_exist kubectl; then
+        install_kubectl /usr/local/bin/kubectl
+    fi
+}
+
+get_file() {
+    url="$1"
+    filename="$2"
+    wget -O "$dest_dir/$filename" $url
+    chmod +x $dest_dir/$filename
+}
+
+main() {
+    prepare_path
+    precheck
+
+    get_file "https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens" kns
+    get_file "https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx" kctx
+    get_file "https://raw.githubusercontent.com/hiberabyss/dotfiles/master/link/bin/kexe" kexe
+    get_file "https://raw.githubusercontent.com/ahmetb/kubectx/master/utils.bash" utils.bash
+}
+
+main $@
